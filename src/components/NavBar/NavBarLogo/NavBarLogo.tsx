43b9553de544e1153d1592356a40c1f35a495b19
Frank Lam <frank.is.missing@gmail.com>
2019-03-14 23:06:43 -0700

    Completed basic file and routing structure

---
+++ src/components/NavBar/NavBarLogo/NavBarLogo.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import logo from '../../../assets/images/logo.svg';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  animation: ${rotate} infinite 20s linear;
+  height: 5vmin;
+  pointer-events: none;
+`;
+
+const NavBarLogo = () => {
+  return (
+    <div>
+      <AppLogo src={logo}></AppLogo>
+    </div>
+  )
+}
+
+export default NavBarLogo;
---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from "../../components/Button/Button";
+
+const Home = () => {
+  return (
+    <Button/>
+  )
+}
+
+export default Home;
